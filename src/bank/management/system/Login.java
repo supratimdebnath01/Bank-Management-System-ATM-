7ddb8e21b5cd95f02e072c388d5a08986deff9aa
Supratim Debnath <supratimdebnath01@gmail.com>
2026-07-29 15:49:53 +0530

    Initial messages

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,18 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame{
+    
+    Login(){
+        setSize(800, 480);
+        setVisible(true);
+    }
+    
+    public static void main(String args[]){
+    
+        new Login();
+    
+    }
+    
+}
